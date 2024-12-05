5318cd255f374bf2961e029305e73d4035835d91
litc0de <litc0de@posteo.org>
2024-12-05 18:07:40 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<main class="p-4">
+  <div class="card p-4">
+    <form action="">
+      <label class="label">
+        <span>Select</span>
+        <select class="select">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+          <option value="4">Option 4</option>
+          <option value="5">Option 5</option>
+        </select>
+      </label>
+    </form>
+  </div>
+</main>
+
+<style>
+</style>
