ac58715eeddc066842041da739d01c1512b52041
litc0de <litc0de@posteo.org>
2024-12-12 14:49:30 +0100

    add production stuff

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,27 +1,44 @@
-// import { opentelemetry } from "@elysiajs/opentelemetry";
 import { cors } from "@elysiajs/cors";
+import staticPlugin from "@elysiajs/static";
 import { Elysia } from "elysia";
 import { edgedb } from "../dbschema/edgeql-js/imports";
 import { fingerprintRoutes } from "./fingerprint";
 import { surveyRoutes } from "./survey";
 import { userRoutes } from "./user";
 
+const isProd = process.env.NODE_ENV === "production";
 const client = edgedb.createClient();
 
-const app = new Elysia()
-  // .use(opentelemetry())
-  .use(
-    cors({
-      origin: "localhost:5173",
-      credentials: true,
-      methods: ["GET", "POST"],
-    }),
-  )
-  .get("/", () => "Hello Elysia")
-  .use(userRoutes(client))
-  .use(surveyRoutes(client))
-  .use(fingerprintRoutes(client))
-  .listen(3000);
+let app;
+
+if (isProd) {
+  app = new Elysia()
+    .use(
+      staticPlugin({
+        prefix: "",
+        assets: "dist",
+      }),
+    )
+    .get("/", () => Bun.file("dist/index.html"))
+    .use(userRoutes(client))
+    .use(surveyRoutes(client))
+    .use(fingerprintRoutes(client))
+    .listen(3000);
+} else {
+  app = new Elysia()
+    .use(
+      cors({
+        origin: "localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST"],
+      }),
+    )
+    .get("/", () => "hello")
+    .use(userRoutes(client))
+    .use(surveyRoutes(client))
+    .use(fingerprintRoutes(client))
+    .listen(3000);
+}
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
