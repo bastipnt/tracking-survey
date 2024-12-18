b32ba7219b8ad876dafbc376782d5653c54efbe1
litc0de <litc0de@posteo.org>
2024-12-18 10:30:16 +0100

    fix meta tags

--- packages/frontend/src/Meta.svelte
+++ packages/frontend/src/Meta.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-  const meta = {
-    title: "✨Tracking Survey✨",
-    description: "A survey about advertisement and tracking",
-    url: "https://tracking-survey.bastipnt.de",
-    image: "https://tracking-survey.bastipnt.de/Tracking_Survey_Screenshot.png",
-  };
+  import { MetaTags } from "svelte-meta-tags";
 </script>
 
-<svelte:head>
-  <title>{meta.title}</title>
-  <meta name="description" content={meta.description} />
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content={meta.url} />
-  <meta property="og:title" content={meta.title} />
-  <meta property="og:description" content={meta.description} />
-  <meta property="og:image" content={meta.image} />
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:url" content={meta.url} />
-  <meta name="twitter:title" content={meta.title} />
-  <meta name="twitter:description" content={meta.description} />
-  <meta name="twitter:image" content={meta.image} />
-</svelte:head>
+<MetaTags
+  title="✨Tracking Survey✨"
+  titleTemplate="%s"
+  description="A survey about advertisement and tracking."
+  canonical="https://tracking-survey.bastipnt.de"
+  openGraph={{
+    url: "https://tracking-survey.bastipnt.de",
+    title: "✨Tracking Survey✨",
+    description: "A survey about advertisement and tracking.",
+    images: [
+      {
+        url: "https://tracking-survey.bastipnt.de/Tracking_Survey_Screenshot.png",
+        alt: "Screenshot of tracking survey webpage",
+      },
+    ],
+    siteName: "tracking-survey",
+  }}
+  twitter={{
+    cardType: "summary_large_image",
+    title: "✨Tracking Survey✨",
+    description: "A survey about advertisement and tracking.",
+    image: "https://tracking-survey.bastipnt.de/Tracking_Survey_Screenshot.png",
+    imageAlt: "Screenshot of tracking survey webpage",
+  }}
+/>
